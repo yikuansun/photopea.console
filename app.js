b4b85142edabacebe801a20a48ddd2841ea144ff
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-11-22 07:35:54 -0500

    Allow typing

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var input = document.querySelector("#userText");
 input.focus();
 input.addEventListener("blur", function() { this.focus(); });
+var output = document.querySelector("#existingText");
+
+input.addEventListener("keydown", function(e) {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        output.innerText += this.innerText + "\n > ";
+        this.innerText = "";
+    }
+});
