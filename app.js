efdd95d30da2ec669010a71f89bf205bf8f40da1
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-11-22 08:46:48 -0500

    Photopea integration

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var output = document.querySelector("#existingText");
 input.addEventListener("keydown", function(e) {
     if (e.key == "Enter") {
         e.preventDefault();
-        output.innerText += this.innerText + "\n > ";
+        output.innerText += this.innerText + "\n";
+        Photopea.runScript(window.parent, this.innerText).then(function(echoed) {
+            output.innerText += echoed.join("\n") + "\n > ";
+        });
         this.innerText = "";
     }
 });
